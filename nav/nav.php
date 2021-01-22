d5ad8fcf4308140cf2f4791170a2cc16589b312d
Mohammad Ashiqur Rahman <ashiqurrussel2@gmail.com>
2021-01-23 00:02:04 +0100

    git test branch

--- nav/nav.php
+++ nav/nav.php
@@ -4,10 +4,19 @@
             <a class="navbar-brand" href="index.php">My Pizza</a>
         </div>
         <ul class="nav navbar-nav">
-            <li class="active"><a href="index.php">Home</a></li>
-            <li> <a class="nav-link" href="pizza.php">Pizza</a></li>
-            <li> <a class="nav-link" href="ingredient.php">Ingredient</a></li>
-            <li> <a class="nav-link" href="supplier.php">Supplier</a></li>
+            <li <?php if (stripos($_SERVER['REQUEST_URI'], 'index.php') !== false) {echo 'class="active"';}?>>
+                <a href="index.php">Home</a>
+            </li>
+            <li <?php if (stripos($_SERVER['REQUEST_URI'], 'pizza.php') !== false) {echo 'class="active"';}?>>
+                <a class="nav-link" href="pizza.php">Pizza</a>
+            </li>
+            <li <?php if (stripos($_SERVER['REQUEST_URI'], 'ingredient.php') !== false) {echo 'class="active"';}?>>
+                <a class="nav-link" href="ingredient.php">Ingredient</a>
+            </li>
+            <li <?php if (stripos($_SERVER['REQUEST_URI'], 'supplier.php') !== false) {echo 'class="active"';}?>>
+                <a class="nav-link" href="supplier.php">Supplier</a>
+            </li>
+
         </ul>
     </div>
 </nav>
